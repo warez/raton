4113818c5b4cbaed91484b949bcc2cc1a83032d3
DaniloCaruso <danilo.caruso.ge@gmail.com>
2015-10-31 00:44:56 +0000

    Aggiunto scaffolding base per applicativo angularjs

---
+++ ngRatOn/src/app/index.route.ts
@@ -0,0 +1,12 @@
+/** @ngInject */
+export function routerConfig($stateProvider: ng.ui.IStateProvider, $urlRouterProvider: ng.ui.IUrlRouterProvider) {
+  $stateProvider
+    .state('home', {
+      url: '/',
+      templateUrl: 'app/main/main.html',
+      controller: 'MainController',
+      controllerAs: 'main'
+    });
+
+  $urlRouterProvider.otherwise('/');
+}
